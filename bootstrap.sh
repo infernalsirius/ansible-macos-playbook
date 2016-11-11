32ae04db483b580323af68df4ac202b4f7694981
Patrick Meunier <patrick.meunier@me.com>
2016-11-11 17:01:45 -0500

    modified bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+# Setup homebrew
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
-git clone 
+brew update && brew install ansible
+
+ansible -i hosts playbooks/laptop-osx.yml
